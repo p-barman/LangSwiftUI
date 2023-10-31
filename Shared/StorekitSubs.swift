2a580720bd07817f816372f8b4a532f72e1304eb
p-barman <palak.onedirection@gmail.com>
2023-10-31 14:51:07 -0500

    testing subs - not building - commit pre re-clone

---
+++ Shared/StorekitSubs.swift
@@ -0,0 +1,43 @@
+//
+//  StorekitSubs.swift
+//  LangSwiftUI
+//
+//  Created by P on 2023-10-31.
+//
+
+import Foundation
+import StoreKit
+import SwiftUI
+
+
+
+//
+
+
+//struct PontentView: View {
+//    var body: some View {
+//        // just show a single product
+//        ProductView(id: "langai_2999_1m")
+//    }
+//}
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//struct PontentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PontentView()
+//    }
+//}
